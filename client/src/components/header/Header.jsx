191aeafc66e6dbde9b5225d7782bbbc503cba179
Ranjeet <ranjeeturd@gmail.com>
2025-08-17 00:00:40 +0530

    add routing and navigation

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,50 @@
+import { nav } from "../../utils/data";
+import { Link } from "react-router-dom";
+import { IconMenu2 } from "@tabler/icons-react"
+import { useState } from "react";
+
+
+function Header() {
+    const [open, setOpen] = useState(false);
+    return (
+        <div className="relative">
+            <div className="flex relative md:rounded-full bg-white justify-between items-center mt-4 max-w-5xl mx-auto px-8 py-2 shadow-main border border-neutral-200">
+                <div>
+                    logo
+                </div>
+
+                <div className="hidden md:flex items-center gap-5">
+                    {nav.map((item, index) => (
+                        <Link to={item.path} key={index} className="hover:text-[#27ae60] capitalize transition duration-200 ease-linear">{item.text}</Link>
+                    ))}
+                </div>
+
+                <div className="hidden sm:flex">
+                    <button className="mr-4">
+                        <span>2</span>
+                        <span>My List</span>
+                    </button>
+                    <button className="bg-[#27AE60] px-4 py-1">Sign In</button>
+                </div>
+
+                <div onClick={() => setOpen(!open)} className="md:hidden">
+                    <IconMenu2 />
+                </div>
+
+                {
+                    open && (
+                        <div className="absolute inset-x-0 bg-white shadow-main top-15 max-w-[90%] mx-auto transition duration-700 ease-in-out">
+                            <div className="flex md:hidden flex-col items-start gap-4 text-sm p-4 rounded-md">
+                                {nav.map((item, index) => (
+                                    <Link to={item.path} key={index} className="hover:text-[#27ae60] capitalize">{item.text}</Link>
+                                ))}
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Header
