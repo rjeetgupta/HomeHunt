316c71d35e149b510a50bc03e593325b8dded443
Ranjeet <ranjeeturd@gmail.com>
2025-08-22 18:09:52 +0530

    add pre hook for password hashing and hook for compare password using bcrypt

--- server/src/models/User.model.js
+++ server/src/models/User.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema({
     name: {
@@ -35,7 +37,46 @@ const userSchema = new Schema({
     ]
 }, { timestamps: true });
 
+
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            name: this.name,
+            email: this.email,
+            role: this.role
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
 const User = model("User", userSchema);
-
-
 export default User;
