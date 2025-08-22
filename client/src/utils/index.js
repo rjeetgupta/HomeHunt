be83e1abc461b0dd096e1af4ca5d8227f4df7125
Ranjeet <ranjeeturd@gmail.com>
2025-08-22 19:47:01 +0530

    add index page for seperate the import at one place

---
+++ client/src/utils/index.js
@@ -0,0 +1,57 @@
+import h1 from "../assets/icon/h1.png";
+import h2 from "../assets/icon/h2.png";
+import h3 from "../assets/icon/h3.png";
+import h4 from "../assets/icon/h4.png";
+import h5 from "../assets/icon/h6.png";
+
+import proptery_1 from "../assets/recent/p-1.png";
+import proptery_2 from "../assets/recent/p-2.png";
+import proptery_3 from "../assets/recent/p-3.png";
+import proptery_4 from "../assets/recent/p-4.png";
+import proptery_5 from "../assets/recent/p-5.png";
+import proptery_6 from "../assets/recent/p-6.png";
+
+// import all location photo
+
+import city_1 from "../assets/explore/city-1.png";
+import city_2 from "../assets/explore/city-2.png";
+import city_3 from "../assets/explore/city-3.png";
+import city_4 from "../assets/explore/city-4.png";
+import city_5 from "../assets/explore/city-5.png";
+import city_6 from "../assets/explore/city-6.png";
+
+// import team image
+
+import team_1 from "../assets/team/team-1.jpg";
+import team_2 from "../assets/team/team-2.jpg";
+import team_3 from "../assets/team/team-3.jpg";
+import team_4 from "../assets/team/team-4.jpg";
+import team_5 from "../assets/team/team-5.jpg";
+import team_6 from "../assets/team/team-6.jpg";
+
+export {
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    proptery_1,
+    proptery_2,
+    proptery_3,
+    proptery_4,
+    proptery_5,
+    proptery_6,
+    city_1,
+    city_2,
+    city_3,
+    city_4,
+    city_5,
+    city_6,
+
+    team_1,
+    team_2,
+    team_3,
+    team_4,
+    team_5,
+    team_6
+}
