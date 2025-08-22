f96f508a9c91749feb3c6fac2c5095f8bc577884
Ranjeet <ranjeeturd@gmail.com>
2025-08-22 18:11:01 +0530

    import cookie parser and add cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,29 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); // serve static files
+app.use(cookieParser());
+
+// CORS middleware
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// import all routes here
+import healthCheckRoutes from "./routes/healthcheck.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+
+
+// use all routes
+app.use("/api/v1/healthcheck", healthCheckRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 
 
